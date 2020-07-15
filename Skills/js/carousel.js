d9d9d8d11fceb1cc56f3370e64d8885f37e88721
Mariano Timistit <mtimistit@ius.austral.edu.ar>
2020-07-15 15:17:16 -0300

    correciones

--- Skills/js/carousel.js
+++ Skills/js/carousel.js
@@ -1,4 +1,97 @@
-///////////////////////////// CAROUSEL 
+///////////////////////////// CAROUSEL STRUCTURE
+const slideListStructure = document.querySelector(".carousel-slide-list-structure");
+const slideStructure = Array.from(slideListStructure.children);
+const nextButtonStructure = document.querySelector(".carousel-structure_button-right");
+const prevButtonStructure = document.querySelector(".carousel-structure_button-left");
+const indicatorNavStructure = document.querySelector(".carousel-structure-nav");
+const indicatorsStructure = Array.from(indicatorNavStructure.children);
+
+const slideWidthStructure = slideStructure[0].getBoundingClientRect().width;
+
+/// SLIDES ARRANGE
+function setSlidePositionStructure (slideStructure, index) {
+    slideStructure.style.left = slideWidthStructure * index + "px";
+}
+
+slideStructure.forEach(setSlidePositionStructure);
+
+// FUNCTIONS
+
+function moveToSlideStructure (slideListStructure, currentSlideStructure, targetSlideStructure) {
+    slideListStructure.style.transform = "translateX(-" + targetSlideStructure.style.left + ")";
+    currentSlideStructure.classList.remove("current-structure-slide");
+    targetSlideStructure.classList.add("current-structure-slide");
+}
+
+function updateIndicatorsStructure (currentIndicatorStructure, targetIndicatorStructure) {
+    currentIndicatorStructure.classList.remove("current-structure-slide");
+    targetIndicatorStructure.classList.add("current-structure-slide");
+}
+
+function hideShowArrowsStructure (slideStructure, prevButtonStructure, nextButtonStructure, targetIndexStructure) {
+    if (targetIndexStructure === 0) {
+        prevButtonStructure.classList.add("hidden-structure");
+        nextButtonStructure.classList.remove("hidden-structure");
+    } else if (targetIndexStructure === slideStructure.length - 1) {
+        prevButtonStructure.classList.remove("hidden-structure");
+        nextButtonStructure.classList.add("hidden-structure");
+    } else {
+        prevButtonStructure.classList.remove("hidden-structure");
+        nextButtonStructure.classList.remove("hidden-structure");
+    }
+}
+
+/// LEFT BUTTON
+prevButtonStructure.addEventListener("click", movePrevStructure);
+
+function movePrevStructure () {
+    const currentSlideStructure = slideListStructure.querySelector(".current-structure-slide");
+    const prevSlideStructure = currentSlideStructure.previousElementSibling;
+    const currentIndicatorStructure = indicatorNavStructure.querySelector(".current-structure-slide");
+    const prevIndicatorStructure = currentIndicatorStructure.previousElementSibling;
+    const prevIndexStructure = slideStructure.findIndex(diaspo => diaspo === prevSlideStructure);
+
+    moveToSlideStructure(slideListStructure, currentSlideStructure, prevSlideStructure);
+    updateIndicatorsStructure(currentIndicatorStructure, prevIndicatorStructure);
+    hideShowArrowsStructure (slideStructure, prevButtonStructure, nextButtonStructure, prevIndexStructure);
+};
+
+/// RIGHT BUTTON
+nextButtonStructure.addEventListener("click", moveNextStructure);
+
+function moveNextStructure () {
+    const currentSlideStructure = slideListStructure.querySelector(".current-structure-slide");
+    const nextSlideStructure = currentSlideStructure.nextElementSibling;
+    const currentIndicatorStructure = indicatorNavStructure.querySelector(".current-structure-slide");
+    const nextIndicatorStructure = currentIndicatorStructure.nextElementSibling;
+    const nextIndexStructure = slideStructure.findIndex(diaspo => diaspo === nextSlideStructure);
+
+    moveToSlideStructure(slideListStructure, currentSlideStructure, nextSlideStructure);
+    updateIndicatorsStructure(currentIndicatorStructure, nextIndicatorStructure);
+    hideShowArrowsStructure (slideStructure, prevButtonStructure, nextButtonStructure, nextIndexStructure);
+};
+
+/// NAV INDICATORS
+
+indicatorNavStructure.addEventListener("click", e => {
+    const targetIndicatorStructure = e.target.closest("button");
+    
+    if (!targetIndicatorStructure) return;
+
+    const currentSlideStructure = slideListStructure.querySelector(".current-structure-slide");
+    const currentIndicatorStructure = indicatorNavStructure.querySelector(".current-structure-slide");
+    const targetIndexStructure = indicatorsStructure.findIndex(punto => punto === targetIndicatorStructure);
+    const targetSlideStructure = slideStructure[targetIndexStructure];
+
+    moveToSlideStructure(slideListStructure, currentSlideStructure, targetSlideStructure);
+    updateIndicatorsStructure(currentIndicatorStructure, targetIndicatorStructure);
+    hideShowArrowsStructure (slideStructure, prevButtonStructure, nextButtonStructure, targetIndexStructure);
+});
+
+
+
+
+///////////////////////////// CAROUSEL CODE
 const slideList = document.querySelector(".carousel-slide-list");
 const slide = Array.from(slideList.children);
 const nextButton = document.querySelector(".carousel_button-right");
@@ -87,3 +180,5 @@ indicatorNav.addEventListener("click", e => {
     updateIndicators(currentIndicator, targetIndicator);
     hideShowArrows (slide, prevButton, nextButton, targetIndex);
 });
+
+
